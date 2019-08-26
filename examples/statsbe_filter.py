18a6220d6b680a503fe56dc089a428c828294f62
mlg <martinilevi@hotmail.com>
2019-08-26 16:31:19 +0200

    filter stats part 2

--- examples/statsbe_filter.py
+++ examples/statsbe_filter.py
@@ -1,5 +1,9 @@
 from gosec_filter import GosecWarning
 
 the_gosec_filter = set([\
-GosecWarning(error="G101: Potential hardcoded credentials (Confidence: LOW, Severity: HIGH)", code="DM_PARENTAL_CHILD_RULE_TOKEN    = `policy_niji_child`")\
+GosecWarning(error="G101: Potential hardcoded credentials (Confidence: LOW, Severity: HIGH)", code="DM_PARENTAL_CHILD_RULE_TOKEN    = `policy_niji_child`"),\
+GosecWarning(error="G101: Potential hardcoded credentials (Confidence: LOW, Severity: HIGH)", code="DM_MALWARE_RULE_TOKEN           = `policy_niji_malware`"),\
+GosecWarning(error="G101: Potential hardcoded credentials (Confidence: LOW, Severity: HIGH)", code="DM_PHISHING_SKIP_RULE_TOKEN     = `phishing allow user`"),\
+GosecWarning(error="G101: Potential hardcoded credentials (Confidence: LOW, Severity: HIGH)", code="DM_PHISHING_RULE_WEB_WL_TOKEN   = `AntiPhishWebWL`"),\
+GosecWarning(error="G101: Potential hardcoded credentials (Confidence: LOW, Severity: HIGH)", code="DM_PHISHING_RULE_MAIN_WL_TOKEN  = `AntiPhishMailWL"),\
 ])
