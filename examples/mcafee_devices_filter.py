d8dfa3343e97cf6ed74adec3bce0e8fa5d2e2fc1
mlg <martinilevi@hotmail.com>
2019-09-17 13:49:47 +0200

    mcafee devices filter updated

--- examples/mcafee_devices_filter.py
+++ examples/mcafee_devices_filter.py
@@ -1,9 +1,5 @@
 from gosec_filter import GosecWarning
 
-Where: /home/contint/go/src/github.com/Telefonica/niji-mcafee/avdevice/token.go:6
-What : G505: Blacklisted import crypto/sha1: weak cryptographic primitive (Confidence: HIGH, Severity: MEDIUM)
-Code : "crypto/sha1"
-
 the_gosec_filter = set([\
 GosecWarning(location="/home/contint/go/src/github.com/Telefonica/niji-mcafee/avdevice/token.go:6",\
              error="G505: Blacklisted import crypto/sha1: weak cryptographic primitive (Confidence: HIGH, Severity: MEDIUM)",\
