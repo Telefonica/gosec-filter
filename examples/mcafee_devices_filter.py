531324284be9073230227d57e21630203dfe8869
mlg <martinilevi@hotmail.com>
2019-09-17 13:45:47 +0200

    mcafee devices filter updated

--- examples/mcafee_devices_filter.py
+++ examples/mcafee_devices_filter.py
@@ -1,13 +1,11 @@
 from gosec_filter import GosecWarning
 
+Where: /home/contint/go/src/github.com/Telefonica/niji-mcafee/avdevice/token.go:6
+What : G505: Blacklisted import crypto/sha1: weak cryptographic primitive (Confidence: HIGH, Severity: MEDIUM)
+Code : "crypto/sha1"
+
 the_gosec_filter = set([\
-GosecWarning(error="G505: Blacklisted import crypto/sha1: weak cryptographic primitive (Confidence: HIGH, Severity: MEDIUM)",\
-             location="/home/contint/go/src/github.com/Telefonica/niji-blocking/src/niji-blocking/blocking.go:10",\
+GosecWarning(location="/home/contint/go/src/github.com/Telefonica/niji-mcafee/avdevice/token.go:6",\
+             error="G505: Blacklisted import crypto/sha1: weak cryptographic primitive (Confidence: HIGH, Severity: MEDIUM)",\
              code='"crypto/sha1"'),\
-GosecWarning(error="G401: Use of weak cryptographic primitive (Confidence: HIGH, Severity: MEDIUM)",\
-             location="/home/contint/go/src/github.com/Telefonica/niji-blocking/src/niji-blocking/blocking.go:182",\
-             code="md5.New()"),\
-GosecWarning(error="G107: Potential HTTP request made with variable url (Confidence: MEDIUM, Severity: MEDIUM)",\
-             location="/home/contint/go/src/github.com/Telefonica/niji-blocking/src/niji-blocking/cmd/templater/main.go:31",\
-             code="http.Get(sheetURL)"),\
 ])
