02b1377a9f02d7ffcfeba71370d9df193e87120b
mlg <martinilevi@hotmail.com>
2020-04-14 22:44:31 +0200

    fix profile be filter

--- cmd/gosec_filter/__init__.py
+++ cmd/gosec_filter/__init__.py
@@ -1,11 +1,29 @@
+def compareGoProjectFiles(a, b, root):
+    aa = str.split(a, root)
+    bb = str.split(b, root)
+    if len(aa) != 2:
+        return False
+    elif len(bb) != 2:   
+        return False
+
+    return aa[1] == bb[1]
+
+def getRootPart(a, root):
+    aa = str.split(a, root)
+    if len(aa) != 2:
+        return aa
+    return aa[1]
+
 class GosecWarning:
+    fileSplitRoot = ""
     lfile    = ""
     line     = ""
     error    = ""
     code     = ""
     store    = False
-    def __init__(self, location="", error="", code="", store=False):
+    def __init__(self, location="", error="", code="", store=False, fileSplitRoot=""):
         if location != "" :
+            self.fileSplitRoot = fileSplitRoot
             tmp = location.split(":")
             if len(tmp) != 2 :
                 raise Exception("invalid len for location split (%d) location(%s)", len(tmp), location)
@@ -18,9 +36,26 @@ class GosecWarning:
         return "Where: %s\nWhat : %s\nCode : %s\n" % (self.lfile+":"+self.line, self.error, self.code)
     def __eq__(self, other):
         #Note: we dont filter by location
-        return self.lfile == other.lfile and self.line == other.line and self.error == other.error and self.code == other.code
+        if self.fileSplitRoot != "":
+            ret = compareGoProjectFiles(self.lfile, other.lfile, self.fileSplitRoot) and self.line == other.line and self.error == other.error and self.code == other.code
+            print compareGoProjectFiles(self.lfile, other.lfile, self.fileSplitRoot)
+        else:
+            ret = compareGoProjectFiles(self.lfile, other.lfile, "/go/src/") and self.line == other.line and self.error == other.error and self.code == other.code
+            print compareGoProjectFiles(self.lfile, other.lfile, "/go/src/")
+
+        print "fspr..." +  self.fileSplitRoot
+        print "self..." + self.lfile
+        print "other.." + other.lfile
+
+        return ret
+
     def __hash__(self):
         #Note: we dont filter by whole location, just file, may have false positives, control number of filter hits vs filter definitions.
         #h = hash(self.error+self.code)
         #print "hash of \""+self.error+self.code+"\" is "+str(h)
-        return hash(self.lfile+self.error+self.code)
+        return hash(getRootPart(self.lfile, self.fileSplitRoot)+self.error+self.code)
+
+class ProfbeGosecWarning(GosecWarning):
+    def __init__(self, location="", error="", code="", store=False):
+        GosecWarning.__init__(self, location, error, code, store, "/niji-profile/src/")
+
